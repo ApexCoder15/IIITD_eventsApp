0e9726dcf90cfdddc89ccefc9f139a9e196aa51e
kushal19057 <kushal19057@iiitd.ac.in>
2023-04-20 20:01:10 +0530

    Added Bottom Nav Bar. Next step is to integrate the bottom nav bar with existing components by changing them into fragments

---
+++ app/src/main/java/com/example/buzzup/FeedFragment.java
@@ -0,0 +1,19 @@
+package com.example.buzzup;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FeedFragment extends Fragment {
+    public FeedFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_feed, container, false);
+    }
+}
