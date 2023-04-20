0defc347a8bc06a031a3821e3ab256e53d9e2a90
kushal19057 <kushal19057@iiitd.ac.in>
2023-04-20 23:53:46 +0530

    started map part integration.

--- app/src/main/java/com/example/buzzup/MapFragment.java
+++ app/src/main/java/com/example/buzzup/MapFragment.java
@@ -1,19 +1,57 @@
 package com.example.buzzup;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
 public class MapFragment extends Fragment {
+    MapView map;
     public MapFragment() {
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Context ctx = getContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
         return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View viewItem, Bundle savedInstanceState) {
+        map = (MapView) getActivity().findViewById(R.id.mapview);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+        map.setBuiltInZoomControls(false);
+        map.setMultiTouchControls(true);
+
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(10.5);
+        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
+        mapController.setCenter(startPoint);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
     }
 }
