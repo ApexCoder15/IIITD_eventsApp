8dfbbdaae159100fcbf3b4db7e9387c52a091a10
kushal19057 <kushal19057@iiitd.ac.in>
2023-05-01 13:30:29 +0530

    admin profile logout btn added

--- app/src/main/java/com/example/buzzup/AdminProfileFragment.java
+++ app/src/main/java/com/example/buzzup/AdminProfileFragment.java
@@ -1,13 +1,18 @@
 package com.example.buzzup;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class AdminProfileFragment extends Fragment {
+    Button logoutBtn;
     public AdminProfileFragment() {
 
     }
@@ -15,5 +20,15 @@ public class AdminProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_admin_profile, container, false);
+    }
+
+    public void onViewCreated(View itemView, Bundle savedInstanceState) {
+        logoutBtn = getActivity().findViewById(R.id.btn_admin_logout);
+        logoutBtn.setOnClickListener(view->{
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+            getActivity().finish();
+        });
     }
 }
