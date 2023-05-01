8dff2d45a447805043cd354262b39e0ac172f4a6
kushal19057 <kushal19057@iiitd.ac.in>
2023-05-01 11:47:08 +0530

    view event activity added all details, TODO krishna event date time

--- app/src/main/java/com/example/buzzup/ViewEventActivity.java
+++ app/src/main/java/com/example/buzzup/ViewEventActivity.java
@@ -8,8 +8,10 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -22,6 +24,7 @@ import com.squareup.picasso.Picasso;
 import org.w3c.dom.Text;
 
 import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -33,6 +36,10 @@ public class ViewEventActivity extends AppCompatActivity
     TextView eventNameTV;
     TextView eventTagsTV;
     ImageView eventImagesIV;
+    TextView eventDescTV;
+    TextView eventDateTimeTV;
+    TextView eventVenueTV;
+    Button eventViewOnMapBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -48,6 +55,10 @@ public class ViewEventActivity extends AppCompatActivity
         eventNameTV = (TextView)findViewById(R.id.event_name);
         eventTagsTV = (TextView)findViewById(R.id.event_tags);
         eventImagesIV = (ImageView)findViewById(R.id.iv_event_images);
+        eventDescTV = (TextView)findViewById(R.id.tv_event_desc);
+        eventDateTimeTV = (TextView)findViewById(R.id.tv_event_datetime);
+        eventVenueTV = (TextView) findViewById(R.id.tv_event_venue);
+        eventViewOnMapBtn = (Button) findViewById(R.id.btn_view_event_on_map);
 
         db.collection("events").get().addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
@@ -63,6 +74,18 @@ public class ViewEventActivity extends AppCompatActivity
                         List<String> imageUrls = (List<String>) document.get("ImageUrls");
                         String imageUrl = imageUrls.get(0);
                         Picasso.get().load(imageUrl).into(eventImagesIV);
+                        String eventDesc = (String) document.get("Description");
+                        eventDescTV.setText(eventDesc);
+                        String eventVenue = (String) document.get("Venue");
+                        eventVenueTV.setText(new StringBuilder().append("Venue : ").append(eventVenue).toString());
+                        // TODO Krishna
+//                        Timestamp eventDateTime = ((Timestamp) document.get("Time"));
+//                        SimpleDateFormat sfd = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+//                        sfd.format(eventDateTime);
+//                        eventDateTimeTV.setText(new StringBuilder().append("Date & Time : ").append(sfd).toString());
+                        eventViewOnMapBtn.setOnClickListener(view-> {
+                            Toast.makeText(this, "TODO Open Map Fragment...", Toast.LENGTH_SHORT).show();
+                        });
                         break;
                     }
                     else
